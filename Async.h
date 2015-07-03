e815c9b2d03cfc35834e4b09af30634faf06f491
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2015-07-03 14:24:11 -0700

    Reimplement Async() in terms of PackagedTask.

--- Async.h
+++ Async.h
@@ -1,23 +1,16 @@
 #pragma once
 
 #include "Future.h"
+#include "PackagedTask.h"
 #include "SystemScheduler.h"
 
-#include <exception>
 #include <utility>
 
 template<typename Func, typename R = decltype(std::declval<Func>()())>
 Future<R> Async(Func f)
 {
-    Promise<R> p;
-    Future<R> result = p.get_future();
-    auto task = [p = std::move(p), f = std::move(f)]() mutable {
-        try {
-            p.set_value(f());
-        } catch (...) {
-            p.set_exception(std::current_exception());
-        }
-    };
+    PackagedTask<R()> task(std::move(f));
+    Future<R> result = task.get_future();
     SystemScheduler().schedule(std::move(task));
     return result;
 }
